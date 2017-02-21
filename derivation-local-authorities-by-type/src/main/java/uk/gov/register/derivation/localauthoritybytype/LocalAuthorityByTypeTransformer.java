6cca9276acf3924b9b0db185eedd96150186ef0b
Karl Baker <karl.baker@digital.cabinet-office.gov.uk>
2017-02-21 13:35:58 +0000

    Use correct order

--- derivation-local-authorities-by-type/src/main/java/uk/gov/register/derivation/localauthoritybytype/LocalAuthorityByTypeTransformer.java
+++ derivation-local-authorities-by-type/src/main/java/uk/gov/register/derivation/localauthoritybytype/LocalAuthorityByTypeTransformer.java
@@ -13,6 +13,7 @@ import java.time.Instant;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.toMap;
 
@@ -27,8 +28,13 @@ public class LocalAuthorityByTypeTransformer implements RegisterTransformer {
     @Override
     public Set<PartialEntity> transform(Set<PartialEntity> newPartialEntities, Set<PartialEntity> state) {
         final Map<String, PartialEntity> stateMap = state.stream().collect(toMap(PartialEntity::getKey, Function.identity()));
-        AtomicInteger entryNumber = new AtomicInteger(currentMaxEntryNumber(state));
-        newPartialEntities.forEach(newEntity -> newEntity.getEntries().forEach(entry -> {
+        int currentMaxEntryNumber = currentMaxEntryNumber(state);
+        AtomicInteger rollingNumber = new AtomicInteger(0);
+
+        Map<Integer, Entry> entries = newPartialEntities.stream().flatMap(e -> e.getEntries().stream()).collect(Collectors.toMap(e -> e.getEntryNumber(), e -> e));
+        Map<String, String> allLocalAuthorities = new HashMap<>();
+
+        entries.values().forEach(entry -> {
             String laType = (String) entry.getItem().getFields().get(LOCAL_AUTHORITY_TYPE);
             String localAuthority = (String) entry.getItem().getFields().get(LOCAL_AUTHORITY_ENG);
 
@@ -36,58 +42,54 @@ public class LocalAuthorityByTypeTransformer implements RegisterTransformer {
                 List<String> currentLocalAuthorities = (List<String>) stateMap.get(laType).getRecord()
                         .orElseThrow(IllegalStateException::new).getItem().getFields().get(LOCAL_AUTHORITIES);
 
-                if (!currentLocalAuthorities.contains(localAuthority)) {
-                    findEntityByAuthority(localAuthority, state)
-                            .ifPresent(pe -> {
-                                List<String> localAuthList = (List<String>) pe.getRecord()
-                                        .orElseThrow(IllegalStateException::new).getItem().getFields().get(LOCAL_AUTHORITIES);
+                if (allLocalAuthorities.containsKey(localAuthority)) {
+                    PartialEntity pe = stateMap.get(allLocalAuthorities.get(localAuthority));
+                    List<String> localAuthList = (List<String>) pe.getRecord().orElseThrow(IllegalStateException::new)
+                            .getItem().getFields().get(LOCAL_AUTHORITIES);
 
-                                Entry localAuthoritiesEntryWithRemoved = createLocalAuthoritiesEntryRemoving(localAuthority, localAuthList, pe.getKey(), entryNumber);
-                                pe.getEntries().add(localAuthoritiesEntryWithRemoved);
-                            });
-                    Entry newLocalAuthoritiesEntry = createLocalAuthoritiesEntryAdding(localAuthority, currentLocalAuthorities, laType, entryNumber);
-                    stateMap.get(laType).getEntries().add(newLocalAuthoritiesEntry);
-
+                    Entry localAuthoritiesEntryWithRemoved = createLocalAuthoritiesEntryRemoving(localAuthority, localAuthList, allLocalAuthorities, pe.getKey(), entry.getEntryNumber() + currentMaxEntryNumber + rollingNumber.getAndIncrement());
+                    pe.getEntries().add(localAuthoritiesEntryWithRemoved);
                 }
-            } else {
-                stateMap.put(laType, createLaTypeEntity(laType, localAuthority, entryNumber));
-            }
-        }));
-        return new HashSet<>(stateMap.values());
-    }
 
-    private Optional<PartialEntity> findEntityByAuthority(String localAuthority, Set<PartialEntity> state) {
-        return state.stream().filter(e -> ((List<String>) e.getRecord().orElseThrow(IllegalStateException::new)
-                .getItem().getFields().get(LOCAL_AUTHORITIES)).contains(localAuthority)).findFirst();
+                Entry newLocalAuthoritiesEntry = createLocalAuthoritiesEntryAdding(localAuthority, currentLocalAuthorities, allLocalAuthorities, laType, entry.getEntryNumber() + currentMaxEntryNumber + rollingNumber.get());
+                stateMap.get(laType).getEntries().add(newLocalAuthoritiesEntry);
+            } else {
+                stateMap.put(laType, createLaTypeEntity(laType, localAuthority, entry.getEntryNumber() + currentMaxEntryNumber));
+            }
+        });
+
+        return new HashSet<>(stateMap.values());
     }
 
     private int currentMaxEntryNumber(Set<PartialEntity> state) {
         return state.stream().mapToInt(pe -> pe.getEntries().size()).sum();
     }
 
-    private Entry createLocalAuthoritiesEntryAdding(String localAuthority, List<String> currentLocalAuthorities, String localAuthorityType, AtomicInteger entryNumber) {
+    private Entry createLocalAuthoritiesEntryAdding(String localAuthority, List<String> currentLocalAuthorities, Map<String, String> allLocalAuthorities, String localAuthorityType, int entryNumber) {
         List<String> updatedLocalAuthorities = new ArrayList<>(currentLocalAuthorities);
         updatedLocalAuthorities.add(localAuthority);
+        allLocalAuthorities.put(localAuthority, localAuthorityType);
         return createLocalAuthoritiesEntry(updatedLocalAuthorities, localAuthorityType, entryNumber);
     }
 
-    private Entry createLocalAuthoritiesEntryRemoving(String localAuthority, List<String> currentLocalAuthorities, String localAuthorityType, AtomicInteger entryNumber) {
+    private Entry createLocalAuthoritiesEntryRemoving(String localAuthority, List<String> currentLocalAuthorities, Map<String, String> allLocalAuthorities, String localAuthorityType, int entryNumber) {
         List<String> updatedLocalAuthorities = new ArrayList<>(currentLocalAuthorities);
         updatedLocalAuthorities.remove(localAuthority);
+        allLocalAuthorities.remove(localAuthority);
         return createLocalAuthoritiesEntry(updatedLocalAuthorities, localAuthorityType, entryNumber);
     }
 
-    private Entry createLocalAuthoritiesEntry(List<String> newLocalAuthorities, String localAuthorityType, AtomicInteger entryNumber) {
+    private Entry createLocalAuthoritiesEntry(List<String> newLocalAuthorities, String localAuthorityType, int entryNumber) {
         Map<String, Object> newFields = new HashMap<>();
         newFields.put(LOCAL_AUTHORITY_TYPE, localAuthorityType);
         newFields.put(LOCAL_AUTHORITIES, newLocalAuthorities);
         Item newLocalAuthoritiesItem = new Item(newFields);
-        Entry newLocalAuthoritiesEntry = new Entry(entryNumber.getAndIncrement(), Instant.now(), hashValue(newFields));
+        Entry newLocalAuthoritiesEntry = new Entry(entryNumber, Instant.now(), hashValue(newFields));
         newLocalAuthoritiesEntry.setItem(newLocalAuthoritiesItem);
         return newLocalAuthoritiesEntry;
     }
 
-    private PartialEntity createLaTypeEntity(String laType, String localAuth, AtomicInteger entryNumber) {
+    private PartialEntity createLaTypeEntity(String laType, String localAuth, int entryNumber) {
         PartialEntity laTypeEntity = new PartialEntity(laType);
         Map<String, Object> fields = new HashMap<>();
         fields.put(LOCAL_AUTHORITY_TYPE, laType);
@@ -95,7 +97,7 @@ public class LocalAuthorityByTypeTransformer implements RegisterTransformer {
         localAuthList.add(localAuth);
         fields.put(LOCAL_AUTHORITIES, localAuthList);
         Item item = new Item(fields);
-        Entry entry = new Entry(entryNumber.getAndIncrement(), Instant.now(), hashValue(fields));
+        Entry entry = new Entry(entryNumber, Instant.now(), hashValue(fields));
         entry.setItem(item);
         laTypeEntity.getEntries().add(entry);
         return laTypeEntity;
