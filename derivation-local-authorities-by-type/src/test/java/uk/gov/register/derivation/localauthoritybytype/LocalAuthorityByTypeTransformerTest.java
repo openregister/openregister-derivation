6710084615ee507fb27bb4baaaed29aad1caef00
John Ollier <john.ollier@digital.cabinet-office.gov.uk>
2017-02-22 17:17:28 +0000

    Add test for entry numbering.

--- derivation-local-authorities-by-type/src/test/java/uk/gov/register/derivation/localauthoritybytype/LocalAuthorityByTypeTransformerTest.java
+++ derivation-local-authorities-by-type/src/test/java/uk/gov/register/derivation/localauthoritybytype/LocalAuthorityByTypeTransformerTest.java
@@ -163,6 +163,81 @@ public class LocalAuthorityByTypeTransformerTest {
         assertThat(localAuthoritiesCty, is(empty()));
     }
 
+    @Test
+    public void shouldPreserveEntryOrder() throws Exception {
+        Set<PartialEntity> update = new HashSet<>();
+
+        PartialEntity leeEntity = new PartialEntity("LEE");
+        List<Entry> leeEntries = leeEntity.getEntries();
+        update.add(leeEntity);
+
+        PartialEntity hasEntity = new PartialEntity("HAS");
+        List<Entry> hasEntries = hasEntity.getEntries();
+        update.add(hasEntity);
+
+        PartialEntity wsxEntity = new PartialEntity("WSX");
+        List<Entry> wsxEntries = wsxEntity.getEntries();
+        update.add(wsxEntity);
+
+        PartialEntity wyeEntity = new PartialEntity("WYE");
+        List<Entry> wyeEntries = wyeEntity.getEntries();
+        update.add(wyeEntity);
+
+        PartialEntity horEntity = new PartialEntity("HOR");
+        List<Entry> horEntries = horEntity.getEntries();
+        update.add(horEntity);
+
+        PartialEntity worEntity = new PartialEntity("WOR");
+        List<Entry> worEntries = worEntity.getEntries();
+        update.add(worEntity);
+
+        PartialEntity wawEntity = new PartialEntity("WAW");
+        List<Entry> wawEntries = wawEntity.getEntries();
+        update.add(wawEntity);
+
+        // entry 1
+        leeEntries.add(createLocalAuthEntry("LEE", "NMD", "Lewes", 1));
+        // entry 2
+        hasEntries.add( createLocalAuthEntry("HAS", "NMD", "Hastings", 2));
+        // entry 3
+        wsxEntries.add( createLocalAuthEntry("WSX", "CTY", "West Sussex", 3));
+        // entry 4
+        wyeEntries.add( createLocalAuthEntry("WYE", "NMD", "Wyre Forest", 4));
+        // entry 5
+        horEntries.add( createLocalAuthEntry("HOR", "NMD", "Horsham", 5));
+        // entry 6
+        worEntries.add( createLocalAuthEntry("WOR", "NMD", "Worcester", 6));
+        // no entry
+        worEntries.add(createLocalAuthEntry("WOR", "NMD", "New Worcester", 7));
+        // entry 7
+        wawEntries.add( createLocalAuthEntry("WAW", "NMD", "Warwick", 8));
+        // entries 8 and 9
+        leeEntries.add(createLocalAuthEntry("LEE", "CTY", "Lewes", 9));
+
+        Set<PartialEntity> state = Collections.emptySet();
+        LocalAuthorityByTypeTransformer transformer = new LocalAuthorityByTypeTransformer();
+        Set<PartialEntity> newState = transformer.transform(update, state);
+
+        Map<String, PartialEntity> entityMap = newState.stream().collect(toMap(PartialEntity::getKey, identity()));
+        assertTrue(entityMap.containsKey("NMD"));
+        PartialEntity nmdEntity = entityMap.get("NMD");
+
+        List<Entry> nmdEntityEntries = nmdEntity.getEntries();
+        assertThat( nmdEntityEntries.get(0).getEntryNumber(), is(1));
+        assertThat( nmdEntityEntries.get(1).getEntryNumber(), is(2));
+        assertThat( nmdEntityEntries.get(2).getEntryNumber(), is(4));
+        assertThat( nmdEntityEntries.get(3).getEntryNumber(), is(5));
+        assertThat( nmdEntityEntries.get(4).getEntryNumber(), is(6));
+        assertThat( nmdEntityEntries.get(5).getEntryNumber(), is(7));
+        assertThat( nmdEntityEntries.get(6).getEntryNumber(), is(9));
+
+        assertTrue(entityMap.containsKey("CTY"));
+        PartialEntity ctyEntity = entityMap.get("CTY");
+        List<Entry> ctyEntries = ctyEntity.getEntries();
+        assertThat( ctyEntries.get(0).getEntryNumber(), is(3));
+        assertThat( ctyEntries.get(1).getEntryNumber(), is(8));
+    }
+
     private PartialEntity createDerivationEntity(String localAuthorityKey, String localAuthorityTypeKey) {
         PartialEntity partialEntity = new PartialEntity(localAuthorityTypeKey);
         Map<String, Object> fields = new HashMap<>();
@@ -192,6 +267,17 @@ public class LocalAuthorityByTypeTransformerTest {
         entry.setItem(item);
         partialEntity.getEntries().add(entry);
         return partialEntity;
+    }
+
+    private Entry createLocalAuthEntry(String key, String authorityType, String name, int entryNumber ){
+        Map<String, Object> fields = new HashMap<>();
+        fields.put("local-authority-eng", key);
+        fields.put("local-authority-type", authorityType);
+        fields.put("name", name);
+        Item item = new Item(fields);
+        Entry entry = new Entry(entryNumber, Instant.EPOCH, "sha-256:zzz");
+        entry.setItem(item);
+        return entry;
     }
 
 }
