4b22e44a58fa73e61760b3814621b64a41e66bb7
Karl Baker <karl.baker@digital.cabinet-office.gov.uk>
2017-02-24 17:30:28 +0000

    Tidy up grouping

--- derivation-generic/src/main/java/uk/gov/register/derivation/generic/groupings/CountryByFirstLetterGrouping.java
+++ derivation-generic/src/main/java/uk/gov/register/derivation/generic/groupings/CountryByFirstLetterGrouping.java
@@ -3,23 +3,19 @@ package uk.gov.register.derivation.generic.groupings;
 import java.util.Optional;
 
 public class CountryByFirstLetterGrouping implements Grouping {
-    private static final String COUNTRIES = "countries";
-    private static final String COUNTRY_CODE = "name";
-    private static final String COUNTRY_NAME = "name";
-
     @Override
     public String getKeyFieldName() {
-        return COUNTRY_CODE;
+        return "name";
     }
 
     @Override
     public String getItemFieldName() {
-        return COUNTRIES;
+        return "countries";
     }
 
     @Override
     public String getItemField() {
-        return COUNTRY_NAME;
+        return "country";
     }
 
     @Override
